1d1a58a99250df36cf440ac51bdcf7ea305a7d7b
niik <151896968+niikseah@users.noreply.github.com>
2026-01-23 14:23:28 +0800

    Added Level 1

--- src/main/java/Ziq.java
+++ src/main/java/Ziq.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Ziq {
     public static void main(String[] args) {
-       System.out.println("Hello I'm Ziq\n" + "What can I do for you?\n" + "Bye! Hope to see you again soon!");
+       System.out.println("Hello I'm Ziq\n" + "What can I do for you?\n");
+
+       Scanner sc = new Scanner(System.in);
+       while (true) {
+           String input = sc.nextLine();
+           if (input.equalsIgnoreCase("bye")) {
+               System.out.println("Bye! Hope to see you again soon!");
+               break;
+           } else {
+               System.out.println(input);
+           }
+       }
     }
 }
