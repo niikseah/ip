13e5151adbf740ce6fd461be1749f253564d6200
niik <151896968+niikseah@users.noreply.github.com>
2026-01-23 14:30:19 +0800

    Added Level 2

--- src/main/java/Ziq.java
+++ src/main/java/Ziq.java
@@ -4,14 +4,24 @@ public class Ziq {
     public static void main(String[] args) {
        System.out.println("Hello I'm Ziq\n" + "What can I do for you?\n");
 
+       String[] list = new String[100];
+       int listCount = 0;
+
        Scanner sc = new Scanner(System.in);
-       while (true) {
+       while (listCount<100) {
            String input = sc.nextLine();
            if (input.equalsIgnoreCase("bye")) {
                System.out.println("Bye! Hope to see you again soon!");
                break;
+           } else if (input.equalsIgnoreCase("list")) {
+               for (int i = 1; i <= listCount; i++) {
+                   System.out.println(i + ". " +
+                           list[i-1]);
+               }
            } else {
-               System.out.println(input);
+               System.out.println("added " + input);
+               list[listCount] = input;
+               listCount++;
            }
        }
     }
