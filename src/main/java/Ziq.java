b1edcb4ad5651598c58deaa9e030178e6199bbca
niik <151896968+niikseah@users.noreply.github.com>
2026-01-23 14:15:08 +0800

    Added Level 0

---
+++ src/main/java/Ziq.java
@@ -0,0 +1,5 @@
+public class Ziq {
+    public static void main(String[] args) {
+       System.out.println("Hello I'm Ziq\n" + "What can I do for you?\n" + "Bye! Hope to see you again soon!");
+    }
+}
