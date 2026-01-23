61dbc7aa30631a0c1b1000f11af054f4cc70f1f1
niik <151896968+niikseah@users.noreply.github.com>
2026-01-23 14:52:30 +0800

    Added Level 3

--- src/main/java/Ziq.java
+++ src/main/java/Ziq.java
@@ -4,7 +4,7 @@ public class Ziq {
     public static void main(String[] args) {
        System.out.println("Hello I'm Ziq\n" + "What can I do for you?\n");
 
-       String[] list = new String[100];
+       Task[] list = new Task[100];
        int listCount = 0;
 
        Scanner sc = new Scanner(System.in);
@@ -14,13 +14,23 @@ public class Ziq {
                System.out.println("Bye! Hope to see you again soon!");
                break;
            } else if (input.equalsIgnoreCase("list")) {
+               System.out.println("Here are the tasks in your list:");
                for (int i = 1; i <= listCount; i++) {
                    System.out.println(i + ". " +
-                           list[i-1]);
+                           list[i - 1]);
                }
-           } else {
+           } else if (input.startsWith("mark ")) {
+               int index = Integer.parseInt(input.substring(5)) - 1;
+               list[index].markAsDone();
+               System.out.println("OK, I've marked this task as done:\n " + list[index]);
+           }  else if (input.startsWith("unmark ")) {
+               int index = Integer.parseInt(input.substring(7)) - 1;
+               list[index].unmark();
+               System.out.println("OK, I've marked this task as not done yet:\n " + list[index]);
+           }
+           else {
                System.out.println("added " + input);
-               list[listCount] = input;
+               list[listCount] = new Task(input);
                listCount++;
            }
        }
