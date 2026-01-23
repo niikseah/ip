3e3c69b14b7d174b7db32d8b42688e1ec9bd0f81
niik <151896968+niikseah@users.noreply.github.com>
2026-01-23 16:00:36 +0800

    Added Level 6

--- src/main/java/Ziq.java
+++ src/main/java/Ziq.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Ziq {
     public static void main(String[] args) {
        System.out.println("Hello I'm Ziq\n" + "What can I do for you?\n");
 
-       Task[] list = new Task[100];
-       int listCount = 0;
+       ArrayList<Task> list = new ArrayList<>();
 
        Scanner sc = new Scanner(System.in);
-       while (listCount<100) {
+       while (list.size()<100) {
            String input = sc.nextLine();
            try {
                if (input.equalsIgnoreCase("bye")) {
@@ -16,35 +16,48 @@ public class Ziq {
                    break;
                } else if (input.equalsIgnoreCase("list")) {
                    System.out.println("Here are the tasks in your list:");
-                   for (int i = 1; i <= listCount; i++) {
+                   for (int i = 1; i <= list.size(); i++) {
                        System.out.println(i + ". " +
-                               list[i - 1]);
+                               list.get(i - 1));
                    }
                } else if (input.startsWith("mark ")) {
                    int index = Integer.parseInt(input.substring(5)) - 1;
-                   list[index].markAsDone();
-                   System.out.println("OK, I've marked this task as done:\n " + list[index]);
+                   list.get(index).markAsDone();
+                   System.out.println("OK, I've marked this task as done:\n " + list.get(index));
                } else if (input.startsWith("unmark ")) {
                    int index = Integer.parseInt(input.substring(7)) - 1;
-                   list[index].unmark();
-                   System.out.println("OK, I've marked this task as not done yet:\n " + list[index]);
+                   list.get(index).unmark();
+                   System.out.println("OK, I've marked this task as not done yet:\n " + list.get(index));
                } else if (input.startsWith("todo ")) {
-                   list[listCount] = handleTodo(input);
-                   listCount++;
-                   System.out.println("Got it. I've added this task:\n " + list[listCount - 1]);
-                   System.out.println("Now you have " + listCount + " tasks in the list.");
+                   list.add(handleTodo(input));
+                   System.out.println("Got it. I've added this task:\n " + list.get(list.size() - 1));
+                   System.out.println("Now you have " + list.size() + " tasks in the list.");
                } else if (input.startsWith("deadline ")) {
-                   list[listCount] = handleDeadline(input);
-                   listCount++;
-                   System.out.println("Got it. I've added this task:\n " + list[listCount - 1]);
-                   System.out.println("Now you have " + listCount + " tasks in the list.");
+                   list.add(handleDeadline(input));
+                   System.out.println("Got it. I've added this task:\n " + list.get(list.size() - 1));
+                   System.out.println("Now you have " + list.size() + " tasks in the list.");
                } else if (input.startsWith("event ")) {
                    String[] parts = input.substring(6).split(" /from | /to ");
-                   list[listCount] = new Event(parts[0], parts[1], parts[2]);
-                   listCount++;
-                   System.out.println("Got it. I've added this task:\n " + list[listCount - 1]);
-                   System.out.println("Now you have " + listCount + " tasks in the list.");
-               } else {
+                   list.add(new Event(parts[0], parts[1], parts[2]));
+                   System.out.println("Got it. I've added this task:\n " + list.get(list.size() - 1));
+                   System.out.println("Now you have " + list.size() + " tasks in the list.");
+               } else if (input.startsWith("delete ")) {
+                   try {
+                       int index = Integer.parseInt(input.substring(7)) - 1;
+
+                       if (index < 0 || index >= list.size()) {
+                           throw new ZiqException("does not exist");
+                       }
+
+                       Task removedTask = list.remove(index);
+
+                       System.out.println("task removed:\n  " + removedTask);
+                       System.out.println("Now you have " + list.size() + " tasks in the list.");
+
+                   } catch (NumberFormatException e) {
+                       throw new ZiqException("Please provide a valid number to delete.");
+                   }
+               }else {
                    throw new ZiqException("idk lol");
            }
        }
