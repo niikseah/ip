30eb512a9091ea5bcc4a5499e2e53f457023ed1b
niik <151896968+niikseah@users.noreply.github.com>
2026-01-30 10:37:31 +0800

    Add Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
-    public String by() {
+    public LocalDateTime by() {
         return this.by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(Ziq.OUTPUT_FORMAT) + ")";
     }
 }
