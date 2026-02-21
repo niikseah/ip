e6a820866343cbedd4436262f491d0efe0d8e942
niik <151896968+niikseah@users.noreply.github.com>
2026-02-21 10:49:46 +0800

    Branch-A-Assertions: fix assert message for null check

--- src/main/java/ziq/Parser.java
+++ src/main/java/ziq/Parser.java
@@ -40,7 +40,7 @@ public class Parser {
      * @throws ZiqException if the command is invalid or cannot be executed
      */
     public static boolean executeCommand(String input, TaskList tasks, Ui ui, Storage storage) throws ZiqException {
-        assert input != null : "input must not be empty";
+        assert input != null : "input must not be null";
         assert tasks != null : "tasks must not be empty";
         assert ui != null : "ui must not be empty";
         assert storage != null : "storage must not be empty";
